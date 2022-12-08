743796953ff1d66a8023dfdcadb427dc47ecd9d5
eugenepokalyuk <eugene.pokalyuk@gmail.com>
2022-12-09 01:43:23 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,33 +4,13 @@ import React from 'react';
 import 'uikit/dist/css/uikit.css'
 import 'uikit/dist/js/uikit.js'
 
-// import { render } from '@testing-library/react';
-
-// class MyComponent extends React.Component {
-//     constructor(props) {
-//       super(props);
-//     }
-
-//     render() {
-//       // change code below this line
-//       return (
-//         <div className='uk-animation-slide-bottom'>
-//             <h1>Hi! This is calculator</h1>
-            
-//             <p className='uk-text'>Very handy and beautiful calculator, the most important thing is that it counts correctly!</p>
-//         </div>
-//       );
-//       // change code above this line
-//     }
-//   };
-
 function App() {
   let result = "";
   let arr = [];
+  
   let operatorArr = ["/","*","-","+",".","="];
 
   const calcValue = (e) => {
-    // e.target.className += "";
     switch (e.target.textContent) {
         case "1":
             result += "1";
@@ -74,12 +54,6 @@ function App() {
             break;
         case "/":
             result += "/";
-            // for (let i = 0; i < operatorArr.length; i++) {
-            //     const element = result.includes(operatorArr[i])
-            //     if(element !== true) {
-            //         result += "/"
-            //     }
-            // }
             break;
         case "*":
             result += "*";
@@ -111,7 +85,6 @@ function App() {
     <div className='uk-container uk-section'>
         <div className="uk-width-expand@s uk-text-center uk-flex uk-flex-center uk-flex-column">
                 <div className='calc-container uk-padding-remove uk-padding-2xsmall' data-uk-scrollspy="cls:uk-animation-fade">
-                    {/* <h1 className='uk-light'>Calc</h1> */}
                     <div className="uk-grid-collapse uk-child-width-expand@s uk-text-center" data-uk-grid>
                         <div className='calc-display'>
                             <div className="uk-padding uk-light uk-text-right uk-height-small">
@@ -122,7 +95,6 @@ function App() {
                     </div>
 
                     <div className="uk-grid-collapse uk-child-width-1-4 uk-text-center" data-uk-grid>
-                    {/* <div className="uk-grid-collapse uk-child-width-expand@s uk-text-center" data-uk-grid> */}
                         <div className='calc-button calc-button-modification' onClick={calcValue}>
                             <div className="uk-padding-small">AC</div>
                         </div>
@@ -132,9 +104,6 @@ function App() {
                         <div className='calc-button calc-button-modification' onClick={calcValue}>
                             <div className="uk-padding-small">C</div>
                         </div>
-                        {/* <div className='calc-button calc-button-modification' onClick={calcValue}>
-                            <div className="uk-padding-small">%</div>
-                        </div> */}
                         <div className='calc-button calc-button-operation' onClick={calcValue}>
                             <div className="uk-padding-small">/</div>
                         </div>
@@ -189,27 +158,17 @@ function App() {
                         <div className='calc-button'>
                             <div className="uk-padding-small"></div>
                         </div>
-                        
                         <div className='calc-button' onClick={calcValue}>
                             <div className="uk-padding-small">0</div>
                         </div>
-
                         <div className='calc-button' onClick={calcValue}>
                             <div className="uk-padding-small">,</div>
                         </div>
-                        
                         <div className='calc-button calc-button-operation' onClick={calcValue} >
                             <div className="uk-padding-small">=</div>
                         </div>
                     </div>
                 </div>
-                {/* <div className='uk-text-left' id="results">
-                        <div className='uk-animation-slide-bottom'>
-                            <h1>Hi! This is calculator</h1>
-                            
-                            <p className='uk-text'>Very handy and beautiful calculator, the most important thing is that it counts correctly!</p>
-                        </div>
-                </div> */}
         </div>
     </div>
   );
